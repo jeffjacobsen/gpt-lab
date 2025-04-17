ac5581118841a3fac0b4025a7b55c6719384c0d2
Jeff Jacobsen <jeff@ms4.com>
2025-04-16 21:45:17 -0600

    wip

--- train_nanogpt.py
+++ train_nanogpt.py
@@ -289,7 +289,7 @@ class Hyperparameters:
     vocab_size = 50257 # should be the tokenizer's size plus any special tokens defined in this script
     # model size - new parameters for GPUs w/ at least 8GB VRAM during testing
     num_layers = 12  # 124m param model should be 12
-    num_heads = 6   # 124m param model should be 6
+    num_heads = 4   # 124m param model should be 6
     model_dim = 1024  # must be divisible by num_heads
     head_dim = None  # if None, will be set to model_dim // num_heads
     mlp_ratio = 4  # 124m param model should be 4
