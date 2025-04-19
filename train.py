8274876455b65867948610b1e2c9399d67bca3f1
Jeff Jacobsen <jeff@ms4.com>
2025-04-19 15:41:57 -0600

    wip

--- train.py
+++ train.py
@@ -118,7 +118,7 @@ class Trainer:
             pos += batch_size
             yield inputs, targets
 
-    def train(args, cli_args, GPT):
+    def train(GPT):
 
         # Check if environment variables are set by torchrun, otherwise default to single GPU
         if "RANK" in os.environ and "WORLD_SIZE" in os.environ and "LOCAL_RANK" in os.environ:
